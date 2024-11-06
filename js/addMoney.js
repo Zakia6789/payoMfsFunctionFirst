2cc7fd37b85f0ab993cf1220358c32d0c43be085
Zakia6789 <junakiinfo23@gmail.com>
2024-11-06 16:41:57 +0600

    first commit

---
+++ js/addMoney.js
@@ -0,0 +1,17 @@
+/**
+ * How to get a number from an input field
+ * 1. create a variable
+ * 2. on the right side document.getElementById(id)
+ * 3. .value
+ * 4. parseFloat
+ */
+document.getElementById('btn-add-money').addEventListener('click', function(event){
+    event.preventDefault();
+
+
+
+
+
+
+
+})
